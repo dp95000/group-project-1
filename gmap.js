2d5a389cad7cb49850179de6fd70422232a2330b
Ian Clark <ianclark226@gmail.com>
2020-01-09 19:55:26 -0500

    working marker map

--- gmap.js
+++ gmap.js
@@ -1,97 +1,81 @@
-//SAVING THIS CODE AS OF RIGHT NOW
-
- //$(document).ready(function(){
-    //  $('#submitWeather').click(function() {
-
-       
-
-//     var area = $("#area").val();
-//     var prices = $("#prices").val();
-
-//     if(area != '') {
-
-    $.ajax({
-
-        url: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCf975ZvBPWoy0r6GlhgTohdNA9RYZM5LQ' + '&callback=initMap',
-       type: "GET",
-        dataType: "jsonp",
-        success: function() {
-//             console.log(data);
-    
-      
-   
-
-
-//             $("#area").val('');
-//             $("#prices").val('');
-//         }
-//     })
-
-// }else{
-//     $("#error").html("<div class= Field cannot be empty</div>");
-// }
-// })
-
-// })
-
-function initMap() {
-  var options = {
+function initMap(){
+    // Map options
+    var options = {
       zoom:8,
       center:{lat:42.3601,lng:-71.0589}
-  }
+    }
 
-  var map = new google.maps.Map(document.getElementById('map'), options);
+    // New map
+    var map = new google.maps.Map(document.getElementById('.map'), options);
 
-  google.maps.event.addListener(map, 'click', function(event) {
+    // Listen for click on map
+    google.maps.event.addListener(map, 'click', function(event){
+      // Add marker
       addMarker({coords:event.latLng});
-  });
+    });
 
-  var markers = [
+    /*
+    // Add marker
+    var marker = new google.maps.Marker({
+      position:{lat:42.4668,lng:-70.9495},
+      map:map,
+      icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+      content:'<h1>Lynn MA</h1>'
+    });
+
+    marker.addListener('click', function(){
+      infoWindow.open(map, marker);
+    });
+    */
+
+    // Array of markers
+    var markers = [
       {
-          coords:{lat:42.4648,lng:-70.9495},
-          iconImage:'https://banner2.cleanpng.com/20180220/flw/kisspng-billboard-advertising-outdoor-billboard-5a8c17c72e21c3.945445991519130567189.jpg',
-          content:'<h1></h1>'
+        coords:{lat:42.4668,lng:-70.9495},
+        iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        content:'<h1>Lynn MA</h1>'
       },
       {
-          coords:{lat:42.8584,lng:-70.9300},
-          content:'<h1></h1>'
+        coords:{lat:42.8584,lng:-70.9300},
+        content:'<h1>Amesbury MA</h1>'
       },
       {
-          coords:{lat:42.7762,lng:-71.0773},  
+        coords:{lat:42.7762,lng:-71.0773}
       }
-  ];
+    ];
 
-  for(var i =0; i< markers.length; i++) {
+    // Loop through markers
+    for(var i = 0;i < markers.length;i++){
+      // Add marker
       addMarker(markers[i]);
-  }
+    }
 
-  function addMarker(props){
-      var marker = new google.maps.Markers({
-          position:props.coords,
-          map:map,
+    // Add Marker Function
+    function addMarker(props){
+      var marker = new google.maps.Marker({
+        position:props.coords,
+        map:map,
+        //icon:props.iconImage
       });
 
-      if(props.iconImage) {
-          marker.setIcon(props.iconImage);
-
+      // Check for customicon
+      if(props.iconImage){
+        // Set icon image
+        marker.setIcon(props.iconImage);
       }
 
-      if(props.content) {
-          var InfoWindow = new google.maps.InfoWindow({
-              content:props.content
-          });
+      // Check content
+      if(props.content){
+        var infoWindow = new google.maps.InfoWindow({
+          content:props.content
+        });
 
-          marker.addListener('click', function(){
-              InfoWindow.open(map, marker);
-          });
+        marker.addListener('click', function(){
+          infoWindow.open(map, marker);
+        });
       }
+    }
   }
-
-      
-
-  };
-
- 
-
-        }
-      })
